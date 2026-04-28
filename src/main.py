fee22d6aaebbea9b44385a4e264a1a3ef81130a2
CH <cedwards2809@gmail.com>
2026-04-28 15:36:38 -0400

    commit

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import sys 
+import vex
+from vex import *
+import math
+
+brain = vex.Brain()
+frontLeftMotor = vex.Motor(vex.Ports.PORT8, vex.GearSetting.RATIO_18_1, False)
+frontRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_18_1, True)
+backLeftMotor = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO_18_1, False)
+backRightMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_18_1, True)
+controller = vex.Controller(vex.ControllerType.PRIMARY)
+reversed = vex.DirectionType.FORWARD
+
+def xDrive(forward, strafe, turn, direction):
+    fl = forward + strafe + turn
+    fr = forward - strafe - turn
+    bl = forward - strafe + turn
+    br = forward + strafe - turn
+    frontLeftMotor.spin(direction, fl, vex.VelocityUnits.PERCENT)
+    frontRightMotor.spin(direction, fr, vex.VelocityUnits.PERCENT)
+    backLeftMotor.spin(direction, bl, vex.VelocityUnits.PERCENT)
+    backRightMotor.spin(direction, br, vex.VelocityUnits.PERCENT)
+
+
+
+
+while True:
+    forward = controller.axis3.value() 
+    strafe = controller.axis4.value()  
+    turn = controller.axis1.value()
+    xDrive(forward, strafe, turn, reversed)
+
+
+        
