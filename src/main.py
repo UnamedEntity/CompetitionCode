67b9874bcf3df08ce93fd78ce5eafd9219a7b946
CH <cedwards2809@gmail.com>
2026-04-28 15:39:23 -0400

    commit

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from vex import *
 import math
 
 brain = vex.Brain()
-frontLeftMotor = vex.Motor(vex.Ports.PORT8, vex.GearSetting.RATIO_18_1, False)
-frontRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_18_1, True)
-backLeftMotor = vex.Motor(vex.Ports.PORT7, vex.GearSetting.RATIO_18_1, False)
-backRightMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_18_1, True)
+frontLeftMotor = vex.Motor(vex.Ports.PORT6, vex.GearSetting.RATIO_18_1, False)
+frontRightMotor = vex.Motor(vex.Ports.PORT19, vex.GearSetting.RATIO_18_1, True)
+backLeftMotor = vex.Motor(vex.Ports.PORT4, vex.GearSetting.RATIO_18_1, False)
+backRightMotor = vex.Motor(vex.Ports.PORT9, vex.GearSetting.RATIO_18_1, True)
 controller = vex.Controller(vex.ControllerType.PRIMARY)
 reversed = vex.DirectionType.FORWARD
 
